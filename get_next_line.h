55b9efc4bd1d810789b86b8cd973e66a8b57c142
shatha-Ajad <sjadala@student.42abudhabi.ae>
2023-02-24 17:34:50 +0400

    FINAL

---
+++ get_next_line.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sjadalla <sjadalla@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/17 15:34:50 by sjadalla          #+#    #+#             */
+/*   Updated: 2022/04/18 16:04:27 by sjadalla         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+char	*ft_get_line(char *str);
+char	*ft_read(int fd, char *static_var);
+char	*ft_strjoin(char *s1, char *s2);
+size_t	ft_strlen(const char *str);
+char	*ft_strchr(const char *s, int c);
+char	*ft_save(char *static_var);
+
+#endif
